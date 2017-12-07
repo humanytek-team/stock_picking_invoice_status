f17d7bcd658e119bc7e85ee3e8c896ee497f659b
Manuel Marquez <buzondemam@gmail.com>
2017-12-07 13:58:15 -0400

    [ADD] Adds invoice status from sale order in document of picking out.

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+### added
+- Adds invoice status from sale order in document of picking out.
---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Humanytek - Manuel Marquez <manuel@humanytek.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Add invoice status in document of pickings out',
+    'version': '9.0.0.1.0',
+    'category': 'Stock',
+    'author': 'Humanytek',
+    'website': "http://www.humanytek.com",
+    'license': 'AGPL-3',
+    'depends': [
+        'stock',
+        'sale',
+        'sale_stock',
+        ],
+    'data': [
+        'views/stock_picking_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
